771aec269efaf7417db04b9247ee7fd285d2ebc1
Austen Allred <austen.allred@gmail.com>
2017-06-02 19:04:21 -0700

    update script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+//1. print the number 5 to the console
+
+//2. Print your name to the console
+
+//3. Store your age as a variable called "myAge"
+
+//4. Print to the console how old you will be in 5 years
+
+//5. Store your favorite food as a variable called "myFavoriteFood"
+
+//6. Publish your favorite food to `index.html` using `document.write()`
+
+//7. Print the remainder of 14 / 3 to the console
+
+//8. Print the remainder of 829 / 13 to the console
+
+//9. Create a for loop that counts from 0 to 130 by 3s
+
+//10. Create a for loop that counts from 3 to 17 by 2s
+
+//11. Create a for loop that counts from 100 to 3 by -1
+
+//12. Create a for loop that counts from 1 to 100 by 1s
+
+//13. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizz` if the number is divisible by 5
+
+//14. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `buzz` if the number is divisible by 3
+
+//15. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizzbuzz` if the number is divisible by 15
+
+//EXTRA CREDIT: Fizzbuzz
+
+/*
+
+The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% of programming job candidates who can't seem to program their way out of a wet paper bag. The text of the programming assignment is as follows:
+
+"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp
+
+*/
